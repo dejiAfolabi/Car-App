004f50913303c26674e4e80e645d5a81a0e4f0e4
dejiAfolabi <dejiafolabi07@gmail.com>
2025-09-01 16:03:40 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,21 @@ import './App.css'
 import Box from '@mui/material/Box';
 import NavBar from './navbar.jsx';
 import Contents from './Content.jsx';
+import Grid from '@mui/material/Grid';
 
 
 function App() {
 
   return (
     <Box sx={{
-       position: "relative",
-        width: "100%",
-        height: '500',
-        overflow: "hidden",
-        borderRadius: 2,
+      position: "relative",
+      width: "100%",
+      height: '500',
+      overflow: "hidden",
+      borderRadius: 2,
     }}>
       <NavBar />
-      <img src='/Images/car.png' style={{ width: '100%', height: '500px', objectFit: 'cover' }}/>
+      <img src='/Images/car.png' style={{ width: '100%', height: '500px', objectFit: 'cover' }} />
       <Contents />
     </Box>
   )
